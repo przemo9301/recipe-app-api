055e6979a852a3a1ab465bfb31567aac4b47c1ee
Przmysław Łukowiak <przemeklukowiak@gmail.com>
2025-12-16 17:38:07 +0100

    Add commit ater lesson 35.

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator function 
+"""
+
+def add (x,y):
+    """Add x and y and retrun result."""
+    return x+y
+def substract(x,y):
+    """Substract x from y and recieved result"""
+    return y-x
