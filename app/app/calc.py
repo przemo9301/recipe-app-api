90a1ef7347be7bd694787aae4c7f4475022d480c
Przmysław Łukowiak <przemeklukowiak@gmail.com>
2025-12-17 22:04:50 +0100

    Changes the configuration files due to wait_for_db.py

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,10 +1,13 @@
 """
-Calculator function 
+Calculator function
 """
 
-def add (x,y):
+
+def add(x, y):
     """Add x and y and retrun result."""
     return x+y
-def substract(x,y):
+
+
+def substract(x, y):
     """Substract x from y and recieved result"""
     return y-x
